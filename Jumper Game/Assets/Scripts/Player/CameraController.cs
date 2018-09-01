c60ec8774e607063c541c97963de2d7a6d1fd025
Luminight <balazs922@gmail.hu>
2018-09-01 22:21:00 +0200

    Player Controller
    Added Camera/Player Controller

---
+++ Jumper Game/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	Vector3 positionOffset;
+	public Transform lookAt;
+
+	public Vector2 rotationSpeed;
+	public bool inverseX = true;
+	public bool inverseY = false;
+
+	public bool mouseInput = false;
+
+	[Space]
+	public float MaxRotation = 45f;
+	public float MinRotation = 340f;
+
+	
+	Vector2 GetAxisInputs(string horizontal, string vertical, bool isRaw = false) {
+		return (isRaw) ? new Vector2(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical)) : new Vector2(Input.GetAxis(horizontal), Input.GetAxis(vertical));
+	}
+
+	Vector2 CameraInput {
+		get {
+			Vector2 input = GetAxisInputs("Horizontal2", "Vertical2");
+			if(input == Vector2.zero && mouseInput)
+				input = GetAxisInputs("Mouse X", "Mouse Y");
+			return input;
+		}
+	}
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		positionOffset = lookAt.transform.position - transform.position;
+	}
+
+	void Update () {
+		transform.position = lookAt.position - positionOffset;
+		Look();
+	}
+	/// <summary>
+	/// Called every frame, Rotates the camera around the player when input is given;
+	/// </summary>
+	void Look() {
+
+		if(CameraInput == Vector2.zero)
+			return;
+
+		Vector3 rotation = transform.eulerAngles;
+
+		rotation.y += CameraInput.x * rotationSpeed.x * Time.deltaTime * (inverseX ? -1 : 1);
+		rotation.x += CameraInput.y * rotationSpeed.y * Time.deltaTime * (inverseY ? -1 : 1);
+
+		Debug.Log(rotation);
+		if(rotation.x < 180)
+			rotation.x = Mathf.Min(MaxRotation, rotation.x);
+
+		if(rotation.x > 180)
+			rotation.x = Mathf.Max(MinRotation, rotation.x);
+
+		transform.eulerAngles = rotation;
+
+
+
+	}
+}
---
+++ Jumper Game/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Transform cameraTransform;
+	public float speed = 500f;
+	public float rotationSpeed = 120f;
+
+	public float jumpSize = 10f;
+
+	private Rigidbody rigid;
+
+	Vector2 MoveInput {
+		get { return GetAxisInputs("Horizontal", "Vertical"); }
+	}
+	
+	Vector2 GetAxisInputs(string horizontal, string vertical, bool isRaw = false) {
+		return (isRaw) ? new Vector2(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical)) : new Vector2(Input.GetAxis(horizontal), Input.GetAxis(vertical));
+	}
+
+	// Use this for initialization
+	void Awake () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+		Jump();
+	}
+
+	void Jump() {
+		Vector3 veloc = rigid.velocity;
+
+		if(rigid.velocity.y > 0 && !Input.GetButton("Jump")) {
+			veloc.y *= 0.8f;
+		}
+
+		if(Input.GetButtonDown("Jump"))
+			veloc.y = jumpSize;
+		
+		rigid.velocity = veloc;
+	}
+
+	void Move() {
+		
+		Vector3 forward = Vector3.Normalize(transform.position - cameraTransform.position);
+		forward.y = 0.0f;
+		Vector3 right = new Vector3(forward.z, 0.0f, -forward.x);
+
+		Vector3 velocity = Vector3.Normalize((forward * MoveInput.y) + (right * MoveInput.x)) * Time.deltaTime * speed;
+
+		if(velocity != Vector3.zero)
+			transform.rotation = Quaternion.LookRotation(velocity, Vector3.up);
+
+		velocity.y = rigid.velocity.y;
+		rigid.velocity = velocity;
+
+		Debug.DrawRay(transform.position, velocity, Color.red);
+	}
+}
