c6cea4cecb87083ea3baa19c83bd38807b874319
Luminight <balazs922@gmail.hu>
2018-09-09 19:59:08 +0200

    Folder Organization + Wall Climbing 0.1
    Organized Folders
    
    Wall Climbing
     - detect walls and climb it
    
    Todo: Wall jump

--- Jumper Game/Assets/Scripts/Player/PlayerController.cs
+++ Jumper Game/Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	#region Variables
+
 	[Header("Components")]
 	public Collider defaultCollider;
 
@@ -22,7 +24,7 @@ public class PlayerController : MonoBehaviour {
 	public bool isGrounded = false;
 	[Tooltip("Color: Yellow")]
 	public Vector3 groundBoxOffset;
-	public Vector3 boxSize;
+	public Vector3 groundBoxSize;
 
 	[Header("Crouch")]
 	public float speedModifier;
@@ -35,8 +37,23 @@ public class PlayerController : MonoBehaviour {
 	public float DiveTime = 0.5f;
 	private MoveForce currentDive;
 
+
+	[Header("Wall Climb")]
+	public ClimbState wallClimb;
+	public enum ClimbState { None, Climbing, Sliding }
+	public float ClimbSpeed;
+	public float ClimbDistance;
+	public float LastWallAngle;
+	public LayerMask wallLayers;
+	public float wallDistance = 0.2f;
+	public Vector3[] wallOffsets = new Vector3[0];
+	private float climbStartY = 0.0f;
+	public float wallSlideSpeed;
+
+
 	[Header("Debug")]
 	public bool isInputLog;
+	public bool isAxisLog;
 
 
 	private Rigidbody rigid;
@@ -46,7 +63,23 @@ public class PlayerController : MonoBehaviour {
 	Vector2 MoveInput {
 		get { return GetAxisInputs("Horizontal", "Vertical"); }
 	}
+
+	Vector3 forward {
+		get { 
+			Vector3 vec = transform.position - cameraTransform.position;
+			vec.y = 0.0f;
+			vec = Vector3.Normalize(vec);
+			return vec;
+		}
+	}
+	Vector3 right {
+		get { 
+			return new Vector3(forward.z, 0.0f, -forward.x);
+		}
+	}
 	
+	#endregion
+
 	Vector2 GetAxisInputs(string horizontal, string vertical, bool isRaw = false) {
 		return (isRaw) ? new Vector2(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical)) : new Vector2(Input.GetAxis(horizontal), Input.GetAxis(vertical));
 	}
@@ -61,12 +94,22 @@ public class PlayerController : MonoBehaviour {
 		Move();
 		Jump();
 		Dive();
+		WallClimb();
 
 		ApplyForces(Time.deltaTime);
 		// Used for debugging purposes (Shows the current input in console)
 		LogInput();
+	}
 
+	void LogInput() {
+		if(!isInputLog)
+			return;
+		if(Input.inputString != "")
+		Debug.Log(Input.inputString);
 
+		for(int i = 0; i < 20; i++)
+			if(Input.GetKeyDown("joystick 1 button " + i))
+				Debug.Log("joystick 1 button " + i);
 	}
 
 	void ApplyForces(float time) {
@@ -80,6 +123,57 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		rigid.velocity = velocity;
+	}
+
+	void WallClimb() {
+		Vector3 way = rigid.velocity;
+		
+		switch(wallClimb) {
+			case ClimbState.None:
+				if(way.y >= 0.0f || isGrounded)
+					return;
+
+				way.y = 0.0f;
+				way.Normalize();
+				
+				bool foundWall = false;
+
+				for(int i = 0; i < wallOffsets.Length && !foundWall; i++) {
+					Vector3 pos = transform.forward * wallOffsets[i].z;
+					pos.y = wallOffsets[i].y;
+					pos += transform.position;
+					foundWall = foundWall || Physics.Raycast(pos, transform.forward, wallDistance, wallLayers.value);
+					Debug.DrawRay(pos, transform.forward * wallDistance, Color.green, 1.0f);
+				}
+
+				if(!foundWall)
+					return;
+
+				wallClimb = ClimbState.Climbing;
+				climbStartY = transform.position.y;
+				LastWallAngle = transform.eulerAngles.y;
+
+			break;
+			case ClimbState.Climbing:
+
+				rigid.velocity = Vector3.up * ClimbSpeed * Time.deltaTime;
+
+				if(Mathf.Abs(transform.position.y - climbStartY) > ClimbDistance)
+					wallClimb = ClimbState.Sliding;
+
+			break;
+			case ClimbState.Sliding:
+				if(isGrounded)
+					wallClimb = ClimbState.None;
+
+				rigid.velocity = Vector3.down * wallSlideSpeed * Time.deltaTime;
+
+			break;
+			default:
+			break;
+		}
+
+
 	}
 
 	void Dive() {
@@ -112,17 +206,6 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
-	void LogInput() {
-		if(!isInputLog)
-			return;
-		if(Input.inputString != "")
-		Debug.Log(Input.inputString);
-
-		for(int i = 0; i < 20; i++)
-			if(Input.GetKeyDown("joystick 1 button " + i))
-				Debug.Log("joystick 1 button " + i);
-	}
-
 	void Jump() {
 		Vector3 veloc = rigid.velocity;
 
@@ -139,7 +222,9 @@ public class PlayerController : MonoBehaviour {
 		
 		rigid.velocity = veloc;
 
-		if(Physics.OverlapBox(transform.position + groundBoxOffset, boxSize, Quaternion.identity, groundLayers.value).Length > 0) {
+		Collider[] cols = Physics.OverlapBox(transform.position + groundBoxOffset, groundBoxSize / 2.0f, Quaternion.identity, groundLayers.value);
+		
+		if(cols.Length > 0) {
 			if(veloc.y <= 0) {
 				currentJumpCount = 0;
 				isGrounded = true;
@@ -152,21 +237,7 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
-	/// <summary>
-	/// Callback to draw gizmos only if the object is selected.
-	/// </summary>
-	void OnDrawGizmosSelected()
-	{
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawWireCube(transform.position + groundBoxOffset, boxSize);
-	}
-
 	void Move() {
-		
-		Vector3 forward = transform.position - cameraTransform.position;
-		forward.y = 0.0f;
-		forward = Vector3.Normalize(forward);
-		Vector3 right = new Vector3(forward.z, 0.0f, -forward.x);
 
 		Vector2 input = MoveInput;
 
@@ -183,7 +254,7 @@ public class PlayerController : MonoBehaviour {
 		velocity.y = rigid.velocity.y;
 		rigid.velocity = velocity;
 
-		if(input != Vector2.zero)
+		if(input != Vector2.zero && isAxisLog)
 			Debug.Log(input);
 
 		Debug.DrawRay(transform.position, velocity, Color.red);
@@ -244,5 +315,19 @@ public class PlayerController : MonoBehaviour {
 			return newVelocity;
 		}
 
+	}
+
+
+	/// <summary>
+	/// Callback to draw gizmos only if the object is selected.
+	/// </summary>
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(transform.position + groundBoxOffset, groundBoxSize);
+		Gizmos.color = Color.blue;
+
+		for(int i = 0; i < wallOffsets.Length; i++)
+			Gizmos.DrawRay(transform.position + wallOffsets[i], transform.forward * wallDistance);
 	}
 }
